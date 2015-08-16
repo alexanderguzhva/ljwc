072087946bb7d6e5432916867f64610d6c7e9425
Alexander Guzhva <alexanderguzhva@gmail.com>
2015-08-16 16:53:58 -0400

    andy's implementation of autoresetevent

---
+++ ljwc-common/src/main/java/com/gschw/lj/common/AutoResetEvent2.java
@@ -0,0 +1,42 @@
+package com.gschw.lj.common;
+
+/**
+ * Created by mileslux on 8/16/15.
+ */
+public class AutoResetEvent2 implements IResetEvent {
+
+    private final Object lockObj = new Object();
+    private boolean isSet = false;
+
+    public AutoResetEvent2(boolean isSet) {
+        this.isSet = isSet;
+    }
+
+    public void waitOne() throws InterruptedException {
+        synchronized (lockObj) {
+            while (!isSet) lockObj.wait();
+            isSet = false;
+        }
+    }
+
+    public boolean waitOne(long millis) throws InterruptedException {
+        synchronized (lockObj) {
+            if (isSet) return !(isSet = false);
+            lockObj.wait(millis);
+            return isSet ? !(isSet = false) : isSet;
+        }
+    }
+
+    public void set() {
+        synchronized (lockObj) {
+            isSet = true;
+            lockObj.notify();
+        }
+    }
+
+    public void reset() {
+        synchronized (lockObj) {
+            isSet = false;
+        }
+    }
+}

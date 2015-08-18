c223a30cfadc86b0a85033b3a27c14a2a609f6f9
Alexander Guzhva <alexanderguzhva@gmail.com>
2015-08-18 09:48:27 -0400

    removed unused projects

--- ljwc-ljdbconvert/src/main/java/com/gschw/ljwc/lj/dbwriter/core/Converter.java
+++
@@ -1,7 +0,0 @@
-package com.gschw.ljwc.lj.dbwriter.core;
-
-/**
- * Created by nop on 7/8/15.
- */
-public class Converter {
-}
--- ljwc-ljdbwriter/src/main/java/com/gschw/ljwc/lj/dbwriter/Main.java
+++
@@ -1,7 +0,0 @@
-package com.gschw.ljwc.lj.dbwriter;
-
-/**
- * Created by nop on 7/6/15.
- */
-public class Main {
-}

2db8d731c47c2b8de40e1839c9bf42cdf5b3b4d2
Alexander Guzhva <alexanderguzhva@gmail.com>
2015-08-21 15:56:22 -0400

    ljexecutor

---
+++ ljwc-ljexecutor/src/main/java/com/gschw/ljwc/lj/executor/DWApplication.java
@@ -0,0 +1,64 @@
+package com.gschw.ljwc.lj.executor;
+
+import com.gschw.ljwc.lj.ljscheduler.client.LJCalendarTaskClient;
+import com.gschw.ljwc.lj.ljscheduler.client.LJSinglePageTaskClient;
+import com.gschw.ljwc.lj.ljscheduler.client.LJTaskClientParameters;
+import io.dropwizard.Application;
+import io.dropwizard.client.JerseyClientBuilder;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.glassfish.jersey.client.ClientProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.client.Client;
+
+/**
+ * Created by hadoop on 8/21/15.
+ */
+public class DWApplication extends Application<DWConfiguration> {
+
+    private static Logger logger = LoggerFactory.getLogger(DWApplication.class);
+
+    @Override
+    public String getName() {
+        return "ljwc-ljexecutor";
+    }
+
+    @Override
+    public void initialize(Bootstrap<DWConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor()
+                )
+        );
+    }
+
+
+    @Override
+    public void run(DWConfiguration configuration, Environment environment) throws Exception {
+        final Client client = new JerseyClientBuilder(environment)
+                .using(configuration.getJerseyClientConfiguration())
+                .build(getName());
+
+        client.property(ClientProperties.CONNECT_TIMEOUT, 1000000);
+        client.property(ClientProperties.READ_TIMEOUT, 1000000);
+
+        //
+        LJTaskClientParameters ljSinglePageClientParameters =
+                configuration.getLjSinglePageClientParameters();
+        LJSinglePageTaskClient singlePageTaskClient =
+                new LJSinglePageTaskClient(client, ljSinglePageClientParameters);
+
+        LJTaskClientParameters ljCalendarClientParameters =
+                configuration.getLjCalendarClientParameters();
+        LJCalendarTaskClient calendarTaskClient =
+                new LJCalendarTaskClient(client, ljSinglePageClientParameters);
+
+        
+
+    }
+}

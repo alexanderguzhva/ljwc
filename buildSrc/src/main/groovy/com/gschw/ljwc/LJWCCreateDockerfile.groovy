282dfba32e24a05eb4e2ec15d97b6013d784dd8c
Alexander Guzhva <alexanderguzhva@gmail.com>
2015-08-30 13:02:53 -0400

    gradle plugin started breathing

--- buildSrc/src/main/groovy/com/gschw/ljwc/LJWCCreateDockerfile.groovy
+++ buildSrc/src/main/groovy/com/gschw/ljwc/LJWCCreateDockerfile.groovy
@@ -1,6 +1,7 @@
 package com.gschw.ljwc
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.TaskAction
 
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
@@ -9,16 +10,25 @@ import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 /**
  * Created by hadoop on 8/27/15.
  */
+
 class LJWCCreateDockerfile extends Dockerfile {
-    @TaskAction
-    void process() {
-        destFile = project.file("$buildDir/docker/Dockerfile")
+    LJWCCreateDockerfile() {
+        destFile = project.file("${project.buildDir}/docker/Dockerfile")
         from 'gschw/busybox-java'
         maintainer 'aguzhva'
         addFile "${project.name}-${project.version}.tar", "/"
         entryPoint "/${project.name}-${project.version}/bin/${project.name}","server", "/${project.name}-${project.version}/config/application.yaml"
     }
 }
+
+class LJWCCopyDistributionToDocker extends Copy {
+    LJWCCopyDistributionToDocker() {
+        from "${project.distsDir}/${project.name}-${project.version}.tar"
+        into "${project.buildDir}/docker"
+    }
+}
+
+
 
 class GreetingTask extends DefaultTask {
     @TaskAction
